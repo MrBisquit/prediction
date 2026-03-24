15dbcce044bf45ee56722e20141dc379164ae10e
Samma - PixelStudio <81291107+Samma2009@users.noreply.github.com>
2026-03-24 21:45:19 +0100

    base

---
+++ Predictor/WeatherNet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TorchSharp.torch.nn;
+using static TorchSharp.torch;
+using TorchSharp.Modules;
+
+namespace Predictor
+{
+    public class WeatherNet : Module<Tensor, Tensor>
+    {
+        private readonly Linear fc1, fc2, fc3;
+
+        public WeatherNet() : base("WeatherNet")
+        {
+            fc1 = Linear(1, 128);
+            fc2 = Linear(128, 256);
+            fc3 = Linear(256, 1);
+            RegisterComponents();
+        }
+
+        public override Tensor forward(Tensor x)
+        {
+            // this is pretty much a placeholder...
+            x = functional.leaky_relu(fc1.forward(x));
+            x = functional.gelu(fc2.forward(x));
+            x = functional.gelu(fc3.forward(x));
+            return x;
+        }
+    }
+}
